4b5ec96e49f0d740dfe04b4e3152d1f93dfbbebe
yeseul choi <yschoi9930@gmail.com>
2021-07-28 02:01:09 +0900

    Implementation

--- Greedy/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙.py
@@ -9,17 +9,17 @@ print(data)
 first = data[n-1] # 가장 큰 수
 second = data[n-2] # 두 번째로 큰 수
 
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k+1)) * k + m % (k+1)
+last = m - count
+
+result = first * count + second * last
+print(result)
+
+# 모범답안
+count = int(m / (k+1)) * k
+count += m % (k+1)
 result = 0
-
-while True:
-    for i in range(k) :
-        if m == 0 :
-            break
-        result += first
-        m -= 1
-    if m==0 :
-        break
-    result += second
-    m -= 1
-
+result += count * first
+result += (m-count) * second
 print(result)
---
+++ Implementation/상하좌우.py
@@ -0,0 +1,21 @@
+N = input()
+x,y = 1,1
+plans = input().split
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+move_types = ['L','R','U','D']
+
+for plan in plans :
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)) :
+        if plan == move_types[i] :
+            nx = x + dx[i]
+            ny = y +dx[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n :
+        continue
+
+    x, y = nx, ny
+
+print(x, y)
